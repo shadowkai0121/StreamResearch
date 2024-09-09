9e6b7d38d0e97fe2cff17f46a76c29c8b4e48e59
Kai Hsieh <shadowkai0121@gmail.com>
2024-09-09 22:55:48 +0800

    add summary

--- summary.py
+++ summary.py
@@ -1,34 +1,45 @@
 import os
-from dotenv import load_dotenv
 from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
+from env_ import Env_
 
-load_dotenv()
+class Summary:
+    client = OpenAI(api_key=os.getenv('GPT_API_KEY'))
+    _env: Env_
+    _messages: str
+    response = None
 
-apikey = os.getenv('GPT_API_KEY')
+    def __init__(self, env_: Env_, messages: str) -> None:
+        self._env = env_
+        self._messages = messages
 
-client = OpenAI(api_key=apikey)
+    def get(self) -> ChatCompletion:
+        response = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {
+                    "role": "system",
+                    "content": '''
+        You will be provided with youtube chat messages, and your task is to summarize the message as follows:
 
-with open('data/≪ENigmatic Recollection≫ ...they found me/01-33_01-49_chat.csv', 'r', encoding='utf8') as f:
-    messages = f.read().strip()
+        -Overall summary of discussion
+        -Situation (the chat's sentiment and the liver is doing what)
+        '''.strip()
+                },
+                {
+                    "role": "user",
+                    "content": self._messages
+                }
+            ],
+            max_tokens=512,
+        )
+        self.response = response
+        return self.response
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {
-            "role": "system",
-            "content": '''
-You will be provided with youtube chat messages, and your task is to summarize the message as follows:
-
--Overall summary of discussion
--Situation (the chat's sentiment and the liver is doing what)
-'''.strip()
-        },
-        {
-            "role": "user",
-            "content": messages
-        }
-    ],
-    max_tokens=512,
-)
-with open('res.json', 'w', encoding='utf8') as f:
-    f.write(response.to_json())
+    def getContent(self) -> str:
+        if self.response is not None:
+            return self.response.choices[0].message.content
+        else:
+            return self.get().choices[0].message.content
+# with open('res.json', 'w', encoding='utf8') as f:
+#     f.write(response.to_json())
